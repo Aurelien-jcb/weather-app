77e6989b70d5e01c6feb0d13238b97c7838590e8
Aurelien-jcb <jacob-aurelien@hotmail.fr>
2021-01-25 16:15:21 +0100

    Modification du fichier App.js afin de résoudre le problème de recherche par ville. Ajout de la date du jour dans le fichier TodayWeather.js. Moficication du css pour l'affichage des prévisions météo.

--- src/App.js
+++ src/App.js
@@ -7,52 +7,52 @@ import OtherdayWeather from './OtherdayWeather';
 
 export default function App() {
 
-    const [weather, setWeather] = useState();
-    const [currentWeather, setCurrentWeather] = useState();
-    const [city, setCity] = useState('');
-    const [newCity, setNewCity] = useState('Lens');
-    const [data, setData] = useState();
-    const [urlCity, setUrlCity] = useState();
-    const [urlWeather, setUrlWeather] = useState();
+  const [weather, setWeather] = useState();
+  const [currentWeather, setCurrentWeather] = useState();
+  const [city, setCity] = useState('');
+  const [newCity, setNewCity] = useState('Lens');
+  const [data, setData] = useState();
+  const [urlCity, setUrlCity] = useState();
+  const [urlWeather, setUrlWeather] = useState();
 
-    // useEffect(() => {
-    //   axios.get('http://api.openweathermap.org/data/2.5/weather?q=lens&appid=ef6e6acf960b100b476d9774b9ac20a3')
-    //   .then(res => {
-    //     setData(res.data);
-    //   })
-    //   axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=50.4167&lon=2.8333&units=metric&lang=fr&exclude=minutely,hourly,alerts&appid=ef6e6acf960b100b476d9774b9ac20a3')
-    //   .then(res => {
-    //     setWeather(res.data.daily);
-    //     setCurrentWeather(res.data.current);
-    //   })
-    // });
-    const api_call = async e => {
-      e.preventDefault();
-      setUrlCity(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ef6e6acf960b100b476d9774b9ac20a3`)
-      console.log(urlCity);
-      axios.get(urlCity).then(response => {
-        setData(response.data);
-      });
-      console.log(urlWeather);
+  useEffect(() => {
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q=lens&appid=ef6e6acf960b100b476d9774b9ac20a3')
+    .then(res => {
+      setData(res.data);
+    })
+    axios.get('https://api.openweathermap.org/data/2.5/onecall?lat=50.4167&lon=2.8333&units=metric&lang=fr&exclude=minutely,hourly,alerts&appid=ef6e6acf960b100b476d9774b9ac20a3')
+    .then(res => {
+      setWeather(res.data.daily);
+      setCurrentWeather(res.data.current);
+    })
+  },[]);
+  
+  const updateData = e => {
+    e.preventDefault();
+    setNewCity(city);
+    setUrlCity(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ef6e6acf960b100b476d9774b9ac20a3`)
+    console.log('url city' + urlCity);
+    axios.get(urlCity).then(response => {
+      setData(response.data);
       setUrlWeather(`https://api.openweathermap.org/data/2.5/onecall?lat=${data.coord.lat}&lon=${data.coord.lon}&units=metric&lang=fr&exclude=minutely,hourly,alerts&appid=ef6e6acf960b100b476d9774b9ac20a3`)
-      setNewCity(city);
       axios.get(urlWeather).then(response => {
         setWeather(response.data.daily);
         setCurrentWeather(response.data.current);
-      });
-      setCity('');
-    }
+      })
+    });
+    setCity('');
+  }
 
-    function updateCity(event) {
-      setCity(event.target.value);
-    }
-    return (
-      <div className="App">
-        <div className="weather__app">
-          <SearchForm api_call={api_call} city={city} updateCity={updateCity}/>
-          { currentWeather && <TodayWeather weather={currentWeather} city={newCity}/>}
-          { weather && <OtherdayWeather weather={weather} />}
-        </div>
-    </div>
-    )
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  return (
+    <div className="App">
+      <div className="weather__app">
+        <SearchForm api_call={updateData} city={city} updateCity={updateCity}/>
+        { currentWeather && <TodayWeather weather={currentWeather} city={newCity}/>}
+        { weather && <OtherdayWeather weather={weather} />}
+      </div>
+  </div>
+  )
 }
--- src/TodayWeather.js
+++ src/TodayWeather.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './TodayWeather.css';
 
 export default function TodayWeather({weather, city}) {
+  const [currentDate, setCurrenDate] = useState();
+  
+  useEffect(() => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth();
+    const date = today.getDate();
+    const event = new Date(Date.UTC(year,month,date));
+    setCurrenDate(event.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }))
+  },[]);
   return (
     <>
       <div className="weather__current">
         <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt=""/>
         <div className="weather__city">
-          <p className="wheather__date">Aujourd'hui</p>
+          <p className="wheather__date">{currentDate}</p>
           <h1 className="weather__title">{city}</h1>
           <p className="weather__temperature">Temperature : {weather.temp.toFixed(1)} °C</p>
           <p className="weather__description">{weather.weather[0].description}</p>
